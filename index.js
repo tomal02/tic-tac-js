91c8078f172678f1fd03521ebb397e98c4808389
tom.lee1 <tom.lee@thortful.com>
2023-03-31 14:51:18 +0100

    prettier ran against all

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   newGame();
-})
+});
 
 const newGame = () => {
   const cells = document.getElementsByClassName('cell');
 
-  for (let i = 0; i < cells.length; i++){
+  for (let i = 0; i < cells.length; i++) {
     cells[i].innerText = '';
   }
-}
+};
 
- const cellClicked = (cellNumber) => {
-  const cells = document.getElementsByClassName('cell')
-  const targetedCell = cells[cellNumber]
-  if(targetedCell.innerText === ""){
-    targetedCell.innerText = "X"
-    console.log("blank cell")
-  } else if(targetedCell.innerText === "X"){
-    targetedCell.innerText = "O"
-  } else if (targetedCell.innerText === "O"){
-    targetedCell.innerText = ""
+const cellClicked = (cellNumber) => {
+  const cells = document.getElementsByClassName('cell');
+  const targetedCell = cells[cellNumber];
+  if (targetedCell.innerText === '') {
+    targetedCell.innerText = 'X';
+    console.log('blank cell');
+  } else if (targetedCell.innerText === 'X') {
+    targetedCell.innerText = 'O';
+  } else if (targetedCell.innerText === 'O') {
+    targetedCell.innerText = '';
   }
- }
+};
 
- const finishTurn = () => {
-  
- }
+const finishTurn = () => {};
 
- const finishGame = () => {
-
- }
+const finishGame = () => {};
