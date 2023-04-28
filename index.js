62398d8dddc2ab2828b05c25483c0349c4641f1e
tom.lee1 <tom.lee@thortful.com>
2023-04-28 13:50:26 +0100

    adds turn taking

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+/*
+0 = Player 1
+1 = Player 2
+*/
+let turn = 'player1'
+
+const game = {
+  winningState: [
+    //Rows
+    ['X', 'X', 'X'],
+    ['O', 'O', 'O'],
+
+    //Columns
+    ['O', 'O', 'O'],
+    ['O', 'O', 'O'],
+  ]
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   newGame();
 });
@@ -23,6 +41,28 @@ const cellClicked = (cellNumber) => {
   }
 };
 
-const finishTurn = () => {};
+const swapTurn = () => {
+  if(turn === 'player1'){
+    turn = 'player2'
+  }else{
+    turn = 'player1'
+  }
+}
+
+const finishTurn = () => {
+  swapTurn();
+  let main = document.getElementsByClassName('main')[0];
+  let turnHeader = document.getElementsByClassName('turn-header')[0]
+  console.log("🚀 ~ file: index.js:47 ~ finishTurn ~ main:", main)
+  if(turn === 'player1'){
+    main.classList.add('player1')
+    main.classList.remove('player2')
+    turnHeader.innerText = "It's player 1s (red) go!"
+  }else{
+    main.classList.add('player2')
+    main.classList.remove('player1')
+    turnHeader.innerText = "It's player 2s (blue) go!"
+  }
+};
 
 const finishGame = () => {};
